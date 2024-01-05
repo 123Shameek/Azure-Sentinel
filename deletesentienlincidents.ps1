98fd052e4291345307d8ebe2572bf1323d36dbb7
Shameek <100445021+123Shameek@users.noreply.github.com>
2024-01-06 00:04:39 +0530

    Update deletesentienlincidents.ps1

--- deletesentienlincidents.ps1
+++ deletesentienlincidents.ps1
@@ -1,10 +1,10 @@
 //The following script bulk deletes all closed Sentinel incidents older than a certain date
 
-$incident = Get-AzSentinelIncident -resourceGroupName baklol -workspaceName kaleenbhaiya | Where-Object {$_.CreatedTimeUtc -lt (Get-Date).AddDays(-30)}
+$incident = Get-AzSentinelIncident -resourceGroupName <RGName> -workspaceName <WSName> | Where-Object {$_.CreatedTimeUtc -lt (Get-Date).AddDays(-30)}
 $incidentgreaterthan90 = ($incident | Where-Object {$_.Status -eq "Closed"}).Name   
 foreach ($i in $incidentgreaterthan90)
 {
-Remove-AzSentinelIncident -ResourceGroupName baklol -WorkspaceName kaleenbhaiya -Id $i
+Remove-AzSentinelIncident -ResourceGroupName <RGName> -WorkspaceName <WSName> -Id $i
 Write-Host "Incident deleted " -NoNewline
 Write-Host $i
 }
